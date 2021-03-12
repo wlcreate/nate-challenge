055e7328105c84f5711387c6acd635f2aaed2ecd
Waverley Leung <waverley.nyc@gmail.com>
2021-03-12 17:10:51 -0500

    adds jest as dependency for testing

--- backend/routes/url.js
+++ backend/routes/url.js
@@ -1,32 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const https = require('https');
+const cors = require('cors');
 
 router.use(cors());
 router.use(express.json());
 
 router.get('/', (request, response) => {
-    response.send("<h1>This is the backend!</h1>");
-});
+    response.send("<h1>This is the backend!</h1>")
+})
 
 router.post('/url', (request, response) => {
-    let url = request.body.url;
+    let url = request.body.url
+    console.log("request.body.url:", url)
 
     https.get(`${url}`, res => {
-        let data = '';
+        let data = ''
 
         res.on('data', chunk => {
-            data += chunk;
-        });
+            data += chunk
+        })
 
         res.on('end', () => {
-            let urlResponse = JSON.parse(data);
+            let urlResponse = data
 
-            response.send(urlResponse);
-        });
+            response.send(urlResponse)
+        })
     }).on('error', error => {
-        response.send("Error:", error.message);
-    });
-});
+        response.send("Error:", error.message)
+    })
+})
 
 module.exports = router
