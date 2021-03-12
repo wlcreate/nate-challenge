52462c0893e2543b049a3e147998025b4b50ce6a
Waverley Leung <waverley.nyc@gmail.com>
2021-03-12 16:12:39 -0500

    sets up routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000
+const PORT = 3000;
+
+app.use('/', require('./routes/url'));
  
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
