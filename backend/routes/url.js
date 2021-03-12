5e2cb6d611503ec0c60cee6e04c8201e2df196a4
Waverley Leung <waverley.nyc@gmail.com>
2021-03-12 16:21:03 -0500

    adds skeleton for post request

--- backend/routes/url.js
+++ backend/routes/url.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const https = require('https');
+
+router.use(cors());
+router.use(express.json());
 
 router.get('/', (request, response) => {
     response.send("<h1>This is the backend!</h1>");
+});
+
+router.post('/url', (request, response) => {
+    let url = request.body.url;
+
+    https.get(`${url}`, res => {
+        let data = '';
+
+        res.on('data', chunk => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            let urlResponse = JSON.parse(data);
+
+            response.send(urlResponse);
+        });
+    }).on('error', error => {
+        response.send("Error:", error.message);
+    });
 });
 
 module.exports = router
